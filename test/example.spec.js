d37a4500e8938ce7bf160678d1b7671c409febc2
Mark Stuart <mstuart_is@me.com>
2014-10-23 12:00:21 +1300

    Initial generated project

---
+++ test/example.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var should = require('chai').should()
+
+describe('Maths', function () {
+  describe('adding 2 numbers', function () {
+    it('should total correctly', function () {
+      var result = 1 + 2
+      result.should.equal(3)
+    })
+  })
+})
