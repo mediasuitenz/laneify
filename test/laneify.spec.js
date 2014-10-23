9877234b443fa5dcf7090d39c21dffd15c3b6b60
Mark Stuart <mstuart_is@me.com>
2014-10-23 14:06:55 +1300

    Add basic test

---
+++ test/laneify.spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+require('chai').should()
+var laneify = require('../index')
+
+var way = {
+    type: 'Feature',
+    id: 'way/23150691',
+    properties: {
+        highway: 'residential',
+    },
+    geometry: {
+        type: 'LineString',
+        coordinates: [
+            [
+                172.6788568,
+                -43.5289081
+            ],
+            [
+                172.6787559,
+                -43.5288417
+            ],
+            [
+                172.6748169,
+                -43.5262494
+            ]
+        ]
+    }
+}
+
+describe('Laneify', function () {
+  describe('splitting a way into two lanes', function () {
+    var validWay, result
+
+    Given('a way', function () {
+      validWay = way
+    })
+    When('the split function is called', function () {
+      result = laneify.split(way)
+    })
+    Then('the result should have 2 features', function () {
+      result.length.should.be(2)
+    })
+  })
+})
