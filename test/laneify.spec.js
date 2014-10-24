318070df381c4d38a0e176a8a8eaa0767662195e
Mark Stuart <mstuart_is@me.com>
2014-10-24 14:29:53 +1300

    Add tests for one way roads and harden interface

--- test/laneify.spec.js
+++ test/laneify.spec.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var clone = require('clone')
+var catchError = require('catch-error')
 require('chai').should()
+var context = describe
 
 var laneify = require('../index')
+
 
 var way = {
   type: 'Feature',
@@ -30,20 +34,70 @@ var way = {
 }
 
 describe('Laneify', function () {
-  describe('splitting a way into two lanes', function () {
-    var validWay, result
+  describe('splitting a road into two lanes', function () {
+    context('with a valid way supplied', function () {
+      var validWay, result
 
-    Given('a way', function () {
-      validWay = way
+      Given('a way', function () {
+        validWay = way
+      })
+      When('the split function is called', function () {
+        result = laneify.split(way)
+      })
+      Then('the result should be a GeoJSON feature collection', function () {
+        result.type.should.equal('FeatureCollection')
+      })
+      And('the result should have 2 features', function () {
+        result.features.length.should.equal(2)
+      })
     })
-    When('the split function is called', function () {
-      result = laneify.split(way)
+
+    context('handling a one way road', function () {
+      var oneWay, result
+
+      Given('a one way road', function () {
+        oneWay = clone(way)
+        oneWay.properties.oneway = 'yes'
+      })
+      When('the split function is called', function () {
+        result = laneify.split(oneWay)
+      })
+      Then('the result should be a GeoJSON feature collection', function () {
+        result.type.should.equal('FeatureCollection')
+      })
+      And('the result should have 1 feature', function () {
+        result.features.length.should.equal(1)
+      })
     })
-    Then('the result should be a GeoJSON feature collection', function () {
-      result.type.should.equal('FeatureCollection')
+
+    context('with an invalid way supplied', function () {
+      var invalidWay, error
+
+      Given('an invalid way', function () {
+        invalidWay = clone(way)
+        invalidWay.geometry.type = 'Point'
+        delete invalidWay.properties.highway
+      })
+      When('the split function is called', function () {
+        error = catchError({ func: laneify.split, args: [invalidWay] })
+      })
+      Then('it should throw an error', function () {
+        error.should.be.an.instanceof(Error)
+      })
     })
-    And('the result should have 2 features', function () {
-      result.features.length.should.equal(2)
+
+    context('with no way supplied', function () {
+      var invalidWay, error
+
+      Given('an invalid way', function () {
+        invalidWay = undefined
+      })
+      When('the split function is called', function () {
+        error = catchError({ func: laneify.split, args: [invalidWay] })
+      })
+      Then('it should throw an error', function () {
+        error.should.be.an.instanceof(Error)
+      })
     })
   })
 })
