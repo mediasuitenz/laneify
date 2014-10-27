3b0afdd7cbae378649cfa243cc1bdabdd9470353
Mark Stuart <mstuart_is@me.com>
2014-10-28 10:13:48 +1300

    Add test for 'with' and 'against' offsets

--- test/translateSegment.spec.js
+++ test/translateSegment.spec.js
@@ -2,12 +2,12 @@
 
 var geoTools = require('../lib/geoTools')
 require('chai').should()
+var context = describe
 
 var translateSegment = require('../lib/translateSegment')
 
 describe('translateSegment', function () {
-  var line, offset, result
-
+  var line, offset
   Given('a vertical line', function () {
     line = geoTools.createLine(
       geoTools.createPoint(0, 0),
@@ -17,13 +17,32 @@ describe('translateSegment', function () {
   And('an offset', function () {
     offset = 1
   })
-  When('translating `with` the line', function () {
-    result = translateSegment(line, offset, true)
+
+  context('`with` the line', function () {
+    var result
+
+    When('the line is translated', function () {
+      result = translateSegment(line, offset, true)
+    })
+    Then('the resultant line should be translated correctly', function () {
+      result.p1.x.should.equal(-1)
+      result.p2.x.should.equal(-1)
+      result.p1.y.should.equal(line.p1.y)
+      result.p2.y.should.equal(line.p2.y)
+    })
   })
-  Then('the resultant line should be translated correctly', function () {
-    result.p1.x.should.equal(-1)
-    result.p2.x.should.equal(-1)
-    result.p1.y.should.equal(line.p1.y)
-    result.p2.y.should.equal(line.p2.y)
+
+  context('`against` the line', function () {
+    var result
+
+    When('the line is translated', function () {
+      result = translateSegment(line, offset, false)
+    })
+    Then('the resultant line should be translated correctly', function () {
+      result.p1.x.should.equal(1)
+      result.p2.x.should.equal(1)
+      result.p1.y.should.equal(line.p1.y)
+      result.p2.y.should.equal(line.p2.y)
+    })
   })
 })
