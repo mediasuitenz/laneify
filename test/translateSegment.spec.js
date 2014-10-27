0f30de4c7fde25011887bf289850d570746f0dc3
Mark Stuart <mstuart_is@me.com>
2014-10-28 09:36:10 +1300

    Add test for basic segment translation

---
+++ test/translateSegment.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var geoTools = require('../lib/geoTools')
+require('chai').should()
+
+var translateSegment = require('../lib/translateSegment')
+
+describe('translateSegment', function () {
+  var line, offset, result
+
+  Given('a vertical line', function () {
+    line = geoTools.createLine(
+      geoTools.createPoint(0, 0),
+      geoTools.createPoint(0, 1)
+    )
+  })
+  And('an offset', function () {
+    offset = 1
+  })
+  When('translating `with` the line', function () {
+    result = translateSegment(line, offset, true)
+  })
+  Then('the resultant line should be translated correctly', function () {
+    result.p1.x.should.equal(-1)
+    result.p2.x.should.equal(-1)
+    result.p1.y.should.equal(line.p1.y)
+    result.p2.y.should.equal(line.p2.y)
+  })
+})
